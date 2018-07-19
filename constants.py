a5783c4d95a1f43f5c8d05dca613c569fa78c39d
ligain <khokhlov86@gmail.com>
2018-07-19 15:32:21 +0300

    finalized get method

--- constants.py
+++
@@ -1,7 +0,0 @@
-
-
-class HttpCode:
-    OK = (200, 'OK')
-    FORBIDDEN = (403, 'Forbidden')
-    NOT_FOUND = (404, 'Not Found')
-    METHOD_NOT_ALLOWED = (405, 'Method Not Allowed')
--- httpd.py
+++ httpd.py
@@ -1,18 +1,25 @@
 import argparse
 import logging
 import multiprocessing
+import mimetypes
 import os
-import io
 
 from threading import Thread
 from urllib.parse import unquote_plus
 from socketserver import TCPServer, StreamRequestHandler
 from functools import partial
+from string import Template
 
-from constants import HttpCode
 
 HOST = 'localhost'
 PORT = 8080
+
+
+class HttpCode:
+    OK = (200, 'OK')
+    FORBIDDEN = (403, 'Forbidden')
+    NOT_FOUND = (404, 'Not Found')
+    METHOD_NOT_ALLOWED = (405, 'Method Not Allowed')
 
 
 class OtusRequestHandler(StreamRequestHandler):
@@ -84,6 +91,15 @@ class OtusRequestHandler(StreamRequestHandler):
         )
         self.wfile.write(response_header_str.encode("latin-1"))
 
+    def send_response_content(self, content, ctype):
+        self.send_header("Server", self.server_version)
+        self.send_header("Content-Type", ctype)
+        self.send_header("Content-Length", len(content))
+        self.send_header("Connection", "close")
+        self.end_headers()
+        # write response content
+        self.wfile.write(content)
+
     def send_header(self, key, value):
         header_str = "{}: {}\r\n".format(key, value)
         self.wfile.write(header_str.encode("latin-1"))
@@ -98,54 +114,90 @@ class OtusRequestHandler(StreamRequestHandler):
         self.end_headers()
 
     def list_directory(self, dir_path):
-        # if dir_path.endswith("index.html"):
-        #     # get index.html as default dir file
-        #     pass
+        logging.info('Getting contents of folder: {}'.format(dir_path))
+        dir_response_tmpl = Template("""
+        <!DOCTYPE html>
+        <html>
+           <head>
+              <meta charset="utf-8" />
+              <title>Directory listing</title>
+           </head>
+           <body>
+              <h1>Listing of directory $dirname :</h1>
+              <ul style="list-style-type: none;">
+                  <li><a href="$parent_link">./</a></li>
+                  $children
+              </ul>
+              <hr>
+              <i>Server: $server</i>
+           </body>
+        </html>
+        """)
         try:
             dir_list = os.listdir(dir_path)
-            if "index.html" in dir_list:
-                # get index.html as default dir file
-                pass
         except OSError:
+            logging.exception('Error getting directory')
             self.send_error(*HttpCode.NOT_FOUND)
             return
 
-        response_str = "Listing of directory:\n"
+        if "index.html" in dir_list:
+            # get index.html as default dir file
+            full_index_path = os.path.join(dir_path, 'index.html')
+            self.retrieve_file(full_index_path)
+
+        if not dir_list:
+            # 404 error if empty directory
+            logging.info('Empty directory: {}'.format(dir_path))
+            self.send_error(*HttpCode.NOT_FOUND)
+            return
+
+        parent = os.path.split(self.path.rstrip('/'))[0]
+        children = ''
+        for dir_item in dir_list:
+            children += '<li><a href="{item_link}">{item_name}</a></li>'.format(
+                item_link=os.path.join(self.path, dir_item),
+                item_name=dir_item
+            )
+        dir_response = dir_response_tmpl.substitute(
+            dirname=os.path.basename(dir_path),
+            parent_link=parent,
+            children=children,
+            server=self.server_version
+        )
+        self.send_response_header(*HttpCode.OK)
+        self.send_response_content(dir_response.encode(), "text/html")
 
     def retrieve_file(self, file_path):
-        pass
+        logging.info('Getting file: {}'.format(file_path))
+        try:
+            file = open(file_path, 'rb')
+        except IOError:
+            logging.exception('Error reading file')
+            self.send_error(*HttpCode.NOT_FOUND)
+            return
 
-    def guess_contenttype(self, file_path, extensions_map=None):
-        pass
+        file_mime_type, _ = mimetypes.guess_type(file_path)
+        if file_mime_type is None:
+            file_mime_type = "application/octet-stream"
+
+        with file:
+            self.send_response_header(*HttpCode.OK)
+            self.send_response_content(file.read(), file_mime_type)
 
     def get(self):
         logging.info('Processing GET request: {}'.format(self.path))
         converted_path = self.convert_path()
-        response_content = ""
 
         if converted_path is None:
             self.send_error(*HttpCode.NOT_FOUND)
             return
         elif os.path.isdir(converted_path):
-            response_content = self.list_directory(converted_path)
+            self.list_directory(converted_path)
         elif os.path.isfile(converted_path):
-            pass
+            self.retrieve_file(converted_path)
         else:
             self.send_error(*HttpCode.FORBIDDEN)
             return
-
-        if response_content is not None:
-            pass
-
-        self.send_response_header(*HttpCode.OK)
-        self.send_header("Server", self.server_version)
-        self.send_header("Content-Type", "text/plain")
-        self.send_header("Content‐Length", len(response_content))
-        self.send_header("Connection", "close")
-        self.end_headers()
-        # write response content
-        response_bytes = io.BytesIO(b"Hello")
-        self.wfile.write(response_bytes.read())
 
     def head(self):
         logging.info('Processing HEAD request: {}'.format(self.path))
@@ -172,12 +224,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Otus HTTP server')
     parser.add_argument(
         '-r', '--docroot',
-        help='Path to the root server directory',
+        help='Path to the root server directory [default: cwd]',
         default=os.getcwd()
     )
     parser.add_argument(
         '-w', '--workers',
-        help='Number of workers to serve',
+        help='Number of workers to serve [default: num of CPU cores]',
         type=int,
         default=multiprocessing.cpu_count()
     )
