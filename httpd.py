0d47c1c846682192eda28239c0228f9e9d234fad
ligain <khokhlov86@gmail.com>
2018-07-29 19:33:27 +0300

    base thread pool server

--- httpd.py
+++ httpd.py
@@ -1,190 +1,53 @@
 import argparse
+import os
 import logging
 import multiprocessing
-import mimetypes
-import os
-
-from threading import Thread
-from urllib.parse import unquote_plus
-from socketserver import TCPServer, StreamRequestHandler
-from functools import partial
+from socket import AF_INET, SOCK_STREAM, socket, SOL_SOCKET, SO_REUSEADDR
+from concurrent.futures import ThreadPoolExecutor
 
 
-HOST = 'localhost'
-PORT = 8080
-
-
-class HttpCode:
-    OK = (200, 'OK')
-    FORBIDDEN = (403, 'Forbidden')
-    NOT_FOUND = (404, 'Not Found')
-    METHOD_NOT_ALLOWED = (405, 'Method Not Allowed')
-
-
-class OtusRequestHandler(StreamRequestHandler):
+class OtusRequestHandler:
     server_version = 'OTUS server/0.1'
+    http_protocol = 'HTTP/1.1'
 
-    def __init__(self, *args, document_root=None, **kwargs):
-        self.document_root = document_root
-        self.method = ''
-        self.protocol = 'HTTP/1.1'
+    def __init__(self, client_addr, client_sock, document_root=None):
+        self.document_root = document_root if document_root else os.getcwd()
+        self.client_addr = client_addr
+        self.client_sock = client_sock
         self.path = ''
+        self.method = ''
+        self.raw_request = b''
         self.headers = {}
-        super().__init__(*args, **kwargs)
 
     def handle(self):
-        logging.info('Got connection from: %s', self.client_address)
+        logging.info('Got connection from: %s', self.client_addr)
         self.parse_request()
-        if hasattr(self, self.method.lower()):
-            method = getattr(self, self.method.lower())
-            method()
-        else:
-            logging.info('Unknown method: %s', self.method)
-            self.send_error(*HttpCode.METHOD_NOT_ALLOWED)
+
+        self.client_sock.sendall(b'Hello')
+        logging.info('Client connection %s closed', self.client_addr)
+        self.client_sock.close()
 
     def parse_request(self):
-        request_line = str(self.rfile.readline(), 'utf8')
-        self.raw_request = request_line.rstrip('\r\n')
-        parts = self.raw_request.split()
-        if len(parts) == 3:
-            self.method, self.path, self.protocol = parts
-        elif len(parts) == 2:
-            self.method, self.path = parts
-        else:
-            logging.info('Invalid request header')
-
-        # Parse request headers
         while True:
-            line = self.rfile.readline()
-            if line in (b'\r\n', b'\n', b''):
+            msg = self.client_sock.recv(8192)
+            if not msg:
                 break
-            decoded_header_line = line.decode('utf8')
-            decoded_header_line = decoded_header_line.rstrip('\r\n')
-            name, body = decoded_header_line.split(': ')
-            self.headers[name] = body
-
-    def convert_path(self, path=None):
-        """
-        Converts url path to local filesystem path.
-        Reads url path from self.path variable
-        """
-        if path is None:
-            path = self.path
-        path = path.split('?', 1)[0]
-        path = unquote_plus(path)
-        path = os.path.normpath(path)
-        path = path.strip('/')
-        full_path = os.path.join(self.document_root, path)
-        if not os.path.exists(full_path):
-            return
-        return full_path
-
-    def send_response_header(self, code, msg):
-        """
-        Adds first line in HTTP response in format:
-        `protocol version` `http code` `http message`
-        """
-        response_header_str = "{protocol} {code} {msg}\r\n".format(
-            protocol=self.protocol,
-            code=code,
-            msg=msg
-        )
-        self.wfile.write(response_header_str.encode("latin-1"))
-
-    def send_headers(self, code, status, content_length, ctype):
-        self.send_response_header(code, status)
-        self.send_header("Server", self.server_version)
-        self.send_header("Content-Type", ctype)
-        self.send_header("Content-Length", content_length)
-        self.send_header("Connection", "close")
-        self.end_headers()
-
-    def send_header(self, key, value):
-        header_str = "{}: {}\r\n".format(key, value)
-        self.wfile.write(header_str.encode("latin-1"))
-
-    def end_headers(self):
-        self.wfile.write(b"\r\n")
-
-    def send_error(self, code, status):
-        self.send_response_header(code, status)
-        self.send_header("Server", self.server_version)
-        self.send_header("Connection", "close")
-        self.end_headers()
-
-    def list_directory(self, dir_path):
-        try:
-            dir_list = os.listdir(dir_path)
-        except OSError:
-            logging.exception('Error getting directory')
-            self.send_error(*HttpCode.NOT_FOUND)
-            return
-
-        if "index.html" in dir_list:
-            # get index.html as default dir file
-            full_index_path = os.path.join(dir_path, 'index.html')
-            return self.retrieve_file(full_index_path)
-        else:
-            logging.info('Try to get directory w/o index.html')
-            self.send_error(*HttpCode.NOT_FOUND)
-            return
-
-    def retrieve_file(self, file_path):
-        logging.info('Getting file: {}'.format(file_path))
-        try:
-            file = open(file_path, 'rb')
-        except IOError:
-            logging.exception('Error reading file')
-            self.send_error(*HttpCode.NOT_FOUND)
-            return
-
-        file_mime_type, _ = mimetypes.guess_type(file_path)
-        if file_mime_type is None:
-            file_mime_type = "application/octet-stream"
-
-        with file:
-            file_content = file.read()
-            self.send_headers(*HttpCode.OK, len(file_content), file_mime_type)
-            return file_content
-            # self.send_response_header(*HttpCode.OK)
-            # self.send_response_content(file.read(), file_mime_type)
-
-    def process_get_and_head(self):
-        converted_path = self.convert_path()
-
-        if converted_path is None:
-            self.send_error(*HttpCode.NOT_FOUND)
-            return
-        elif os.path.isdir(converted_path):
-            content_bytes = self.list_directory(converted_path)
-        elif os.path.isfile(converted_path):
-            content_bytes = self.retrieve_file(converted_path)
-        else:
-            self.send_error(*HttpCode.FORBIDDEN)
-            return
-        return content_bytes
-
-    def get(self):
-        logging.info('Processing GET request: {}'.format(self.path))
-        content_bytes = self.process_get_and_head()
-        if not content_bytes:
-            logging.error('Sending an empty response in GET request')
-        self.wfile.write(content_bytes)
-
-    def head(self):
-        logging.info('Processing HEAD request: {}'.format(self.path))
-        self.process_get_and_head()
+            self.raw_request += msg
+        print(1)
 
 
-def run_server(serv):
-    """ Run instance of TCP server """
-    host, port = serv.server_address
-    logging.info('Start worker to serve on host: %s, port: %s', host, port)
-    try:
-        serv.serve_forever()
-    except KeyboardInterrupt:
-        logging.info('Keyboard interrupt received, exiting.')
-        serv.shutdown()
+def run_server(address, backlog=5, max_workers=5, document_root=None):
+    pool = ThreadPoolExecutor(max_workers)
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.bind(address)
+    sock.listen(backlog)
+    logging.info('Started OTUS server on: %s with max_workers: %d',
+                 address, max_workers)
+    while True:
+        client_sock, client_addr = sock.accept()
+        request_handler = OtusRequestHandler(client_addr, client_sock, document_root)
+        pool.submit(request_handler.handle)
 
 
 if __name__ == '__main__':
@@ -207,11 +70,8 @@ if __name__ == '__main__':
         default=multiprocessing.cpu_count()
     )
     args = parser.parse_args()
-
-    handler = partial(OtusRequestHandler, document_root=args.docroot)
-    serv = TCPServer((HOST, PORT), handler)
-    for _ in range(args.workers):
-        tread = Thread(target=run_server, args=(serv, ))
-        tread.daemon = True
-        tread.start()
-    run_server(serv)
+    run_server(
+        address=('', 8080),
+        max_workers=args.workers,
+        document_root=args.docroot
+    )
